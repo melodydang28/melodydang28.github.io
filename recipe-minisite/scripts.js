0937eea16271da7bf4435939f23fb024bedd6280
melodydang28 <167152536+melodydang28@users.noreply.github.com>
2025-10-19 18:34:41 -0700

    Fixed typo and added event in JS

---
+++ recipe-minisite/scripts.js
@@ -0,0 +1,8 @@
+document.addEventListener("DOMContentLoaded", function(){
+        //respond to clicks on the burger icon
+        document.querySelector("#navBurger").addEventListener("click", function(e){
+                document.querySelector("nav").classList.toggle("clicked"); 
+        });
+
+
+});
