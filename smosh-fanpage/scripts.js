64f664f62dfd80fb763637f67d158da54f256b54
melodydang28 <167152536+melodydang28@users.noreply.github.com>
2025-12-06 12:13:30 -0800

    added comments

--- smosh-fanpage/scripts.js
+++ smosh-fanpage/scripts.js
@@ -1,8 +1,8 @@
+/*Navigation Burger click event*/
+
 document.addEventListener("DOMContentLoaded", function(){
         //respond to clicks on the burger icon
         document.querySelector("#navBurger").addEventListener("click", function(e){
                 document.querySelector("nav").classList.toggle("clicked"); 
         });
-
-
 });
